7e77cde858b99eaa4029dc534ceab932db28e2dd
Hung Tran <hungleotq@gmail.com>
2024-03-15 23:26:42 +0700

    add retry mechanism

--- src/main/java/checkin/service/CheckInService.java
+++ src/main/java/checkin/service/CheckInService.java
@@ -9,9 +9,15 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Service
@@ -30,34 +36,38 @@ public class CheckInService {
     @Autowired
     MailService mailService;
 
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Retryable(retryFor = Exception.class,
+            backoff = @Backoff(delay = 1000))
     public void checkin() {
-        try {
-            log.info("Start checkin : {}", new Date());
-            RestTemplate restTemplate = new RestTemplate();
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectNode body = mapper.createObjectNode();
-            body.put("badgeNumber", "6888");
-            body.put("password", "seta@123");
-            ResponseEntity<ObjectNode> response = restTemplate.postForEntity(loginUrl, body, ObjectNode.class);
-            log.info("Login: {}\n {}\n {}", loginUrl, body, response);
+        log.info("Start checkin : {}", LocalDateTime.now().format(formatter));
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode body = mapper.createObjectNode();
+        body.put("badgeNumber", "6888");
+        body.put("password", "seta@123");
+        ResponseEntity<ObjectNode> response = restTemplate.postForEntity(loginUrl, body, ObjectNode.class);
+        log.info("Login: {}\n {}\n {}", loginUrl, body, response);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Cookie", response.getHeaders().get("Set-Cookie").get(0) );
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", response.getHeaders().get("Set-Cookie").get(0) );
 
-            body = mapper.createObjectNode();
-            body.put("comment", "");
-            HttpEntity<ObjectNode> entity = new HttpEntity<ObjectNode>(body, headers);
+        body = mapper.createObjectNode();
+        body.put("comment", "");
+        HttpEntity<ObjectNode> entity = new HttpEntity<ObjectNode>(body, headers);
 
-            response = restTemplate.exchange(checkinUrl, HttpMethod.POST, entity, ObjectNode.class);
-            String checkIn = response.getBody().path("data").path("checkIn").asText("");
+        response = restTemplate.exchange(checkinUrl, HttpMethod.POST, entity, ObjectNode.class);
+        String checkIn = response.getBody().path("data").path("checkIn").asText("");
 
-            log.info("CheckIn: {}\n {}\n {}", checkinUrl, body, response);
+        log.info("CheckIn: {}\n {}\n {}", checkinUrl, body, response);
 
-            mailService.sendMail("Checked in at " + checkIn);
-        } catch (Exception ex) {
-            log.error("Error: {}", ex.getMessage());
-            ex.printStackTrace();
-        }
+        mailService.sendMail("Checked in at " + LocalDateTime.now().format(formatter));
     }
 
+    @Recover
+    public void recover(Exception e) {
+        log.error("Error: {}", e.getMessage());
+        mailService.sendMail("Failed to checkin at " + LocalDateTime.now().format(formatter) + " cause by " + e.getMessage());
+    }
 }
